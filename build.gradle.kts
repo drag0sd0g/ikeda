e60a2157ce822c454f7325d0777eaecdbf378ffd
drag0sd0g <dragosdog@gmail.com>
2026-07-31 21:26:42 +0100

    gradle wrapper bootstrap

---
+++ build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    application
+}
+
+repositories {
+    mavenCentral()
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(25)
+    }
+}
+
+application {
+    mainClass = "com.ikeda.Main"
+}
---
+++ src/main/java/com/ikeda/Main.java
@@ -0,0 +1,7 @@
+package com.ikeda;
+
+public class Main {
+    static void main(){
+        System.out.println("Ikeda online " + Runtime.version().feature());
+    }
+}
